5b472ef27da77a00d58c5a3da4983a1a14b017f8
Ozyasar <canozyasar35@gmail.com>
2025-07-06 12:49:11 +0300

    ders 10 kategorik verilerin sayısal verilere dönüşümü ile daha doğru çıktılar alınmasının sağlanması

--- Bolum_2_veri analizi proje yönetimi ve problem tipleri/Ders10_kategorik verile/kategorik_veriler.py
+++ Bolum_2_veri analizi proje yönetimi ve problem tipleri/Ders10_kategorik verile/kategorik_veriler.py
@@ -1,10 +1,12 @@
-# ders9 eksikveriler.py
+# ders10 kategorik_veriler.py
 
 
-# bu de rste bazen veriler içinde eksiklikler olabilir bu verilerin giderilmesi doğru  değerlendirme için daha iyi olur .
+# Bu derste kategorik veriler üzerinde işlem yapacağız. Kategorik veriler, # belirli kategorilere veya sınıflara ayrılabilen verilerdir. Örneğin, cinsiyet, renk, şehir gibi veriler kategorik verilere örnek olarak verilebilir.
+# Kategorik veriler genellikle sayısal olmayan değerler içerir ve bu nedenle # makine öğrenimi algoritmaları tarafından doğrudan işlenemez. Bu nedenle, kategorik verileri sayısal verilere dönüştürmek için etiket kodlama (label encoding) veya tek sıcaklık kodlama (one-hot encoding) gibi teknikler kullanılır.
+# Bu derste, kategorik verileri nasıl işleyebileceğimizi ve bu verileri # makine öğrenimi modellerinde nasıl kullanabileceğimizi öğreneceğiz. Ayrıca, # eksik verileri nasıl doldurabileceğimizi de göreceğiz.
+# Kategorik veriler üzerinde işlem yaparken, verilerin doğru bir şekilde # kodlanması ve eksik verilerin uygun bir şekilde doldurulması önemlidir.
 
-# DERS 9 EKSİK VERİLER cvs dosyasındaki eksik verileri doldurma ve işleme alma işlemleri yapılacak.
-# dosyada eksik veriler nan olarak işaretlenir
+#ÖZETLE kategorik verileri sayısal verilere dönüştürme işlemi yaptık . 
 
 import pandas as pd
 import numpy as np
@@ -13,35 +15,71 @@ import matplotlib.pyplot as plt
 
 
 
-veriler=pd.read_csv('eksikveriler.csv')
-print(veriler)
+veriler=pd.read_csv('kategorikVeriler.csv')
+print(veriler) #  tüm verileri ekrana yazdırırız
 
 
 
 
-# DERS 9 EKSİK VERİLER i tamamlama 
+# DERS 10 kategorik veriler i doğru sınıflandırma 
 
 
 from sklearn.impute import SimpleImputer
 
 
 imputer=SimpleImputer(missing_values=np.nan,strategy ='mean')
-# dosyada nan olan değerleri mean yani ortalamaneyse o olarak yazar günceller 
+ #dosyada nan olan değerleri mean yani ortalamaneyse o olarak yazar günceller 
 
 
 yas=veriler.iloc[:,1:4].values
-print(yas)
-
-#
-imputer=imputer.fit(yas[:,1:4])# bu kısımda fit metodu ile eksik veriler için ortalama değerleri hesaplanır
-
-yas[:,1:4]=imputer.transform(yas[:,1:4]) #transform metodu ile eksik veriler doldurulur
-print(yas) # print ile doldurulmuş veriler yazdırılır
+# print(yas)
 
 
+ulke =veriler.iloc[:,0:1].values
+print(ulke) #ülke kolonunu alırız
+
+from sklearn import preprocessing
+
+le=preprocessing.LabelEncoder()
+
+
+ulke[:,0] = le.fit_transform(veriler.iloc[:,0]) # ulke kolonundaki verileri etiket kodlama ile sayısal verilere dönüştürürüz
+# fit_transform ile hem fit hem de transform işlemini yaparız 
+
+print(ulke)
+# [1]
+#  [1]
+#  [1]
+#  [1]
+#  [1]
+#  [1]
+#  [1]
+#  [1]
+#  [1]
+#  [2]
+#  [2]
+#  [2]
+#  [2]
+#  [2]
+#  [2]
+#  [0]
+#  [0]
+#  [0]
+#  [0] bu şekilde çıktı alırız
+
+
+ohe=preprocessing.OneHotEncoder() #üç kolonumuz vardı bunları tr 1 usa2 vs yazmak yarine tek satırda hangisi ise ona 1 yazacak diyerlerine 0 yazacak 
+ulke=ohe.fit_transform(ulke).toarray()
+print(ulke)
+ # tr usa fr
+# #[0. 1. 0.]
+#  [0. 1. 0.]
+#  [0. 1. 0.]
+#  [0. 1. 0.]
+#  [0. 1. 0.]
+#  [0. 1. 0.]
+#  [0. 1. 0.]
+#  [0. 1. 0.]  bu şekilde çıktı alırız numpy array olarak çıktı alırız
 
     
-
-
-    
-# eksik veriler 
+ 
